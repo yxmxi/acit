e043ffcbed2cc9b39a77f686efab84a254c659ed
yxmxi <baldriasmarijem28@gmail.com>
2026-09-17 19:16:38 -0700

    Week 2 in class

---
+++ Week2/Practice2.py
@@ -0,0 +1,21 @@
+print("this is bcit")
+statement = "bcit is a cool school"
+print(statement)
+school = "bcit"
+statement = "is a cool school"
+school_statement = school + " " + statement
+school_statement = f"{school} really {statement}"
+print(school_statement)
+
+younger_brother_age = 19
+older_sister_age = 20
+print(younger_brother_age + older_sister_age)
+#this program figures out your age 
+current_year = 2026
+birth_year = input("tell me your birth year:")
+birth_year_as_int = int(birth_year)
+print(current_year - int(birth_year))
+#debugging
+#print(type(current_year))
+#print(type(birth_year))
+print(current_year - birth_year_as_int)
---
+++ Week2/Week2inclass.py
@@ -0,0 +1,17 @@
+x = 4
+y = "sleep"
+z = 3.14
+
+x ="hungry"
+y = True
+z = 3.33
+
+print(x)
+print(type(y))
+
+class_name = "ACIT"
+class_number = "1515"
+course_name = class_name + class_number
+print(course_name)
+assignment_number = 2
+print(f'Assignment {assignment_number} created')
